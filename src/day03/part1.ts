85ef0250bf0703bbfe7ccfc5b8aae1789f9b55eb
Aniruddh S. <noirrr@duck.com>
2026-01-04 17:10:40 -0800

    Switch to same stack-based approach as part 2

--- src/day03/part1.ts
+++ src/day03/part1.ts
@@ -2,42 +2,26 @@ export async function main() {
     const banks = (await Bun.file("inputs/day03.txt").text()).trimEnd().split("\n");
 
     let result = 0;
-    const n = banks[0]!.length;
 
     for (const bank of banks) {
-        let best = -1;
-        for (let i = 9; i >= 0; i--) {
-            const digit = i;
-            const indices = [...bank]
-                .map((c, idx) => (c === digit.toString() ? idx : -1))
-                .filter((idx) => idx !== -1);
+        const toDrop = bank.length - 2;
+        const stack = [];
+        let dropped = 0;
 
-            for (const index of indices) {
-                if (index === n - 1) {
-                    continue;
-                }
-
-                const firstDigit = bank[index];
-                const secondDigit = Math.max(
-                    ...bank
-                        .slice(index + 1)
-                        .split("")
-                        .map(Number),
-                );
-
-                const battery = parseInt(firstDigit! + secondDigit, 10);
-
-                if (battery > best) {
-                    // console.log(`Found new best battery ${battery} for bank ${bank}`);
-                    best = battery;
-                }
+        for (const ch of bank) {
+            while (dropped < toDrop && stack.length > 0 && stack[stack.length - 1]! < ch) {
+                stack.pop();
+                dropped++;
             }
-
-            if (best !== -1) {
-                break;
-            }
+            stack.push(ch);
         }
-        result += best;
+
+        while (dropped < toDrop) {
+            stack.pop();
+            dropped++;
+        }
+
+        result += parseInt(stack.join(""), 10);
     }
 
     return result;
